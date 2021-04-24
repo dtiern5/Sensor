1fce074aa66e6d4da92c63145176432500f62411
dtiern5 <dtierney@fastmail.com>
2021-04-24 09:58:06 -0500

    added StandardSensor

---
+++ src/main/java/application/StandardSensor.java
@@ -0,0 +1,31 @@
+package application;
+
+public class StandardSensor implements Sensor {
+
+    private int sensorNumber;
+    
+    public StandardSensor(int number) {
+        this.sensorNumber = number;
+    }
+    
+    @Override
+    public boolean isOn() {
+        return true;
+    }
+
+    @Override
+    public void setOn() {
+        
+    }
+
+    @Override
+    public void setOff() {
+        
+    }
+
+    @Override
+    public int read() {
+        return this.sensorNumber;
+    }
+    
+}
